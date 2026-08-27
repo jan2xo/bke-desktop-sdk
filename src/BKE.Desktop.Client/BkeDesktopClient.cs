eed00e14f52fe1cb7ac7613cdea2bbd31c438c5b
jan2xo <163499503+jan2xo@users.noreply.github.com>
2026-08-27 23:50:06 +0800

    fix: restore explicit framework imports for SDK build

--- src/BKE.Desktop.Client/BkeDesktopClient.cs
+++ src/BKE.Desktop.Client/BkeDesktopClient.cs
@@ -1,7 +1,12 @@
+using System;
 using System.Net;
+using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+
 
 namespace BKE.Desktop.Client;
 
