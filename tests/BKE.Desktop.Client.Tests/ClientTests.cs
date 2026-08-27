efc3d09d16b90421012a18a67b470294981281ab
jan2xo <163499503+jan2xo@users.noreply.github.com>
2026-08-27 23:59:37 +0800

    test: restore framework imports

--- tests/BKE.Desktop.Client.Tests/ClientTests.cs
+++ tests/BKE.Desktop.Client.Tests/ClientTests.cs
@@ -1,4 +1,8 @@
+using System;
 using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Net.Http.Headers;
 using System.Text;
 using BKE.Desktop.Client;
