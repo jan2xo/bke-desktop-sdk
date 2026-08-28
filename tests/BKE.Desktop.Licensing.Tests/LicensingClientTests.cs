6cc2bd41938db8c91b2fadaea99ba0f7e0c31ea9
jan2xo <163499503+jan2xo@users.noreply.github.com>
2026-08-28 23:34:15 +0800

    test: add explicit framework imports

--- tests/BKE.Desktop.Licensing.Tests/LicensingClientTests.cs
+++ tests/BKE.Desktop.Licensing.Tests/LicensingClientTests.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
 using BKE.Desktop.Licensing;
 using Xunit;
 
