7f87859a9de34f4f725a8f4e1c9a5ab10fdee437
jan2xo <163499503+jan2xo@users.noreply.github.com>
2026-08-29 21:29:31 +0800

    fix: make notification contract namespaces explicit

--- src/BKE.Notifications/NotificationContracts.cs
+++ src/BKE.Notifications/NotificationContracts.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace BKE.Notifications;
 
 public enum NotificationSeverity { Information, Success, Warning, Error }
